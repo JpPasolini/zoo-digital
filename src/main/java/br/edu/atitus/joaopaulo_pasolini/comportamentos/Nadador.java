fd887987e4a3461cd13d1690f04b53d6304d94a5
João Paulo <pasolinijp@gmail.com>
2025-11-03 00:23:23 -0300

    "Projeto Finalizado"

--- src/main/java/br/edu/atitus/joaopaulo_pasolini/comportamentos/Nadador.java
+++
@@ -1,5 +0,0 @@
-package br.edu.atitus.joaopaulo_pasolini.comportamentos;
-
-public interface Nadador {
-    void nadar();
-}
--- src/main/java/br/edu/atitus/joaopaulo_pasolini/comportamentos/Predador.java
+++
@@ -1,5 +0,0 @@
-package br.edu.atitus.joaopaulo_pasolini.comportamentos;
-
-public interface Predador {
-    void cacar();
-}
